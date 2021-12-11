fede634e9f71a3579a9da78ebfbb2b1248c5f69e
4ntonin <zenoob04@gmail.com>
2021-12-11 19:20:32 +0100

    amogus txt

--- CultureParty.java
+++ CultureParty.java
@@ -16,7 +16,7 @@ class CultureParty extends Program {
         int nbevents = taille / 2; // on laisse moitié de cases vide et on divise en parts égales pour les events
         for (int i=0;i<nbevents;i++) {
             do {
-                randompos = (int) (random() * (taille - 1) + 1);
+                randompos = (int) (random() * (taille - 2) + 1);
             } while (map[randompos] != VIDE);
             if (i%5 == 0) {
                 map[randompos] = BOOSTER;
@@ -32,7 +32,6 @@ class CultureParty extends Program {
         }
         return map;
     }
-
 
     void afficherMap(char[] map) {
         // ╔╗╚╝═║╦╩╠╣    = caractères utilisables pour la map
@@ -77,40 +76,52 @@ class CultureParty extends Program {
         return (int) (random() * taille + 1);
     }
 
-    void effectuerDeplacement(int deplacement) {
-        joueur.position += deplacement;
+    void effectuerDeplacement(int deplacement, char[] map) {
+        int i = 0, plus = 1, taillemap = length(map);
+        if (deplacement < 0) {
+            plus = -1;
+            deplacement = abs(deplacement);
+        }
+        while (i < deplacement && joueur.position < taillemap-1) {
+            joueur.position += plus;
+            i++;
+            clearTerminal();
+            afficherMap(map);
+            readString();  // A REMPLACER PAR FONCTION TIME pour attendre 0.5sec avant d'avancer
+        }
         if (joueur.position < 0) joueur.position = 0;
     }
 
     void lancerEvent(char[] map) {
         char caseactuelle = map[joueur.position];
         if (caseactuelle == BOOSTER) {
-            eventBooster();
+            eventBooster(map);
         } else if (caseactuelle == RALENTISSEUR) {
-            eventRalentisseur();
+            eventRalentisseur(map);
         } else if (caseactuelle == AJOUTPOINTS) {
             eventAjoutPoints();
         } else if (caseactuelle == SUPPRESSIONPOINTS) {
             eventSupressionPoints();
         } else if (caseactuelle == MINIJEU) {
             eventMiniJeu();
+        } else {
+            eventQuestion();
         }
     }
 
-    void eventBooster() {
+    void eventBooster(char[] map) {
         // dé
         int randint = (int) (random() * 3 + 1);
-        effectuerDeplacement(randint);
+        effectuerDeplacement(randint, map);
     }
 
-    void eventRalentisseur() {
+    void eventRalentisseur(char[] map) {
         // dé
         int randint = (int) (random() * 3 - 3);
-        effectuerDeplacement(randint);
+        effectuerDeplacement(randint, map);
     }
 
-    // PAS ENCORE IMPLANTABLE (donc vide)
-
+    // PAS ENCORE IMPLANTABLES (donc vides)
     void eventAjoutPoints() {
         ;
     }
@@ -123,6 +134,10 @@ class CultureParty extends Program {
         ;
     }
 
+    void eventQuestion() {
+        ; // poser une question si case vide
+    }
+
     void clearTerminal() {
         print("\033[H\033[2J");
         System.out.flush();
@@ -133,8 +148,12 @@ class CultureParty extends Program {
         // PARAMETRAGE
         clearTerminal();
         print("\n\n\n");
+        for (int i=0;i<132/2-13;i++) print(" ");
+        println("══════════════════════════");
         for (int i=0;i<132/2-14;i++) print(" ");
         println("Bienvenue dans Culture Party");
+        for (int i=0;i<132/2-13;i++) print(" ");
+        println("══════════════════════════");
         print("\n\n\n");
 
         for (int i=0;i<132/2-21;i++) print(" ");
@@ -149,9 +168,9 @@ class CultureParty extends Program {
         print("\n\n\n");
         for (int i=0;i<132/2-23;i++) print(" ");
         println("Veuillez sélectionner le nombre de faces du dé\n");
-        for (int i=0;i<132/2-14;i++) print(" ");
+        for (int i=0;i<132/2-8;i++) print(" ");
         println("3       |       6");
-        int taillede = 0;
+        int taillede = 0, resde;
         while (taillede != 3 && taillede != 6) taillede = readInt();
 
         // JEU
@@ -162,10 +181,36 @@ class CultureParty extends Program {
             print("\n\n\n");
             println("Appuyez sur Entrer pour lancer le dé");
             readString();
-            effectuerDeplacement(lancerDe(taillede));
+            print("\n\n");
+            resde = lancerDe(taillede);
+            println("Vous avez fait " + resde + " !");
+            readString();
+            effectuerDeplacement(resde, map);
+            print("Vous êtes tombé sur une case ");
+            if (map[joueur.position] == BOOSTER) {
+                print("Booster!\nVous allez avancer d'un nombre de cases aléatoire.");
+            } else if (map[joueur.position] == RALENTISSEUR) {
+                print("Ralentisseur...\nVous allez reculer d'un nombre de cases aléatoire.");
+            } else if (map[joueur.position] == AJOUTPOINTS) {
+                print("Ajout de points!\nVous allez recevoir un nombre de points aléatoire.");
+            } else if (map[joueur.position] == SUPPRESSIONPOINTS) {
+                print("Supression de points...\nVous allez perdre un nombre de points aléatoire.");
+            } else if (map[joueur.position] == MINIJEU) {
+                print("Mini-Jeu!");
+            } else {
+                print("vide.\nVous devez répondre à une question pour gagner un point!");
+            }
+            readString();
             lancerEvent(map);
+            print("\n\n");
+            readString();
+            if (map[joueur.position] != VIDE) {
+                clearTerminal();
+                afficherMap(map);
+            }
         }
-        afficherMap(map);
+        println("Bravo! Vous avez fini cette partie avec un total de ?? points!");
+
     }
 }
 

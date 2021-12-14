b16edea59aa9f6c2474f1ac25367da8ccea81d77
4ntonin <zenoob04@gmail.com>
2021-12-14 11:29:58 +0100

    Début de la gestion des csv pour les cases vides

--- CultureParty.java
+++ CultureParty.java
@@ -1,14 +1,43 @@
+import extensions.CSVFile;
+
 class CultureParty extends Program {
     final char VIDE = ' ';
     final char BOOSTER = 'B';
     final char RALENTISSEUR = 'R';
     final char MINIJEU = 'J';
     Joueur joueur = new Joueur();
-    // Sans doute mettre les questions dans un csv pour faciliter l'ajout / supression et la lecture / écriture du code
-    QuestionVide[] qvides = new QuestionVide[]{creerQuestionVide("Quel est le nom de la planète sur laquelle nous vivons ?", "La Terre", "Mars", "La Lune", "Le Japon", 'A', "Notre planète s'appelle La Terre."),
-                                            creerQuestionVide("Quel est le nom du satellite naturel de la Terre ?", "Mars", "Jupiter", "La Lune", "Spoutnik", 'C', "Le satellite naturel de la Terre est la Lune."),
-                                            creerQuestionVide("Vrai ou Faux ? Le corps humain est composé à environ 70% d'eau.", "Vrai", "Faux", 'A', "Pour un humain adulte, le corps humain est composé d'environ 70% d'eau.")};
 
+
+    // QuestionVide[] qvides = new QuestionVide[]{creerQuestionVide("Quel est le nom de la planète sur laquelle nous vivons ?", "La Terre", "Mars", "La Lune", "Le Japon", 'A', "Notre planète s'appelle La Terre."),
+    //                                         creerQuestionVide("Quel est le nom du satellite naturel de la Terre ?", "Mars", "Jupiter", "La Lune", "Spoutnik", 'C', "Le satellite naturel de la Terre est la Lune."),
+    //                                         creerQuestionVide("Vrai ou Faux ? Le corps humain est composé à environ 70% d'eau.", "Vrai", "Faux", 'A', "Pour un humain adulte, le corps humain est composé d'environ 70% d'eau.")};
+
+    QuestionVide[] qvides = creerQuestionsCasesVides();
+
+    // UTILISER FONCTION CREER QUESTION
+    QuestionVide[] creerQuestionsCasesVides() {
+        CSVFile csvqvides = loadCSV("./questions/QuestionsVide.csv");
+        int csvqvidesheight = rowCount(csvqvides) - 1;
+        int csvqvideswidth = columnCount(csvqvides);
+        QuestionVide[] qvidebdd = new QuestionVide[csvqvidesheight];
+        for (int i=1;i<csvqvidesheight;i++) {
+            qvidebdd[i].question = getCell(csvqvides, i, 1);
+            if (equals(getCell(csvqvides, i, 3), "")) {
+                qvidebdd[i].choix = new String[2];
+                qvidebdd[i].choix[1] = getCell(csvqvides, i, 1);
+                qvidebdd[i].choix[2] = getCell(csvqvides, i, 2);
+            } else {
+                qvidebdd[i].choix = new String[4];
+                qvidebdd[i].choix[1] = getCell(csvqvides, i, 1);
+                qvidebdd[i].choix[2] = getCell(csvqvides, i, 2);
+                qvidebdd[i].choix[3] = getCell(csvqvides, i, 3);
+                qvidebdd[i].choix[4] = getCell(csvqvides, i, 4);
+            }
+            qvidebdd[i].rep = charAt(getCell(csvqvides, i, 5), 0);
+            qvidebdd[i].explication = getCell(csvqvides, i, 6);
+        }
+        return qvidebdd;
+    } 
 
     char[] creerMapRoute66(int taille) {
         char[] map = new char[taille];
@@ -105,13 +134,55 @@ class CultureParty extends Program {
         if (joueur.position < 0) joueur.position = 0;
     }
 
+    QuestionVide creerQuestionVide(String question, String choix1, String choix2, char rep, String explication) {  // 2 choix
+        QuestionVide q = new QuestionVide();
+        q.question = question;
+        q.choix = new String[2];
+        q.choix[0] = choix1;
+        q.choix[1] = choix2;
+        q.rep = rep;
+        q.explication = explication;
+        return q;
+    }
+
+    QuestionVide creerQuestionVide(String question, String choix1, String choix2, String choix3, String choix4, char rep, String explication) {  // 4 choix
+        QuestionVide q = new QuestionVide();
+        q.question = question;
+        q.choix = new String[4];
+        q.choix[0] = choix1;
+        q.choix[1] = choix2;
+        q.choix[2] = choix3;
+        q.choix[3] = choix4;
+        q.rep = rep;
+        q.explication = explication;
     void affichageCaseActuelle(char[] map) {
         char caseactuelle = map[joueur.position];
         print("\n\nVous êtes tombé sur une case ");
         if (caseactuelle == BOOSTER) {
             print("Booster!\nVous allez avancer d'un nombre de cases aléatoire et gagner des pièces!");
         } else if (caseactuelle == RALENTISSEUR) {
-            print("Ralentisseur...\nVous allez reculer d'un nombre de cases aléatoire.");
+            print("Ralentisseur...\nVous allez reculer d'un nomb
+    QuestionVide creerQuestionVide(String question, String choix1, String choix2, char rep, String explication) {  // 2 choix
+        QuestionVide q = new QuestionVide();
+        q.question = question;
+        q.choix = new String[2];
+        q.choix[0] = choix1;
+        q.choix[1] = choix2;
+        q.rep = rep;
+        q.explication = explication;
+        return q;
+    }
+
+    QuestionVide creerQuestionVide(String question, String choix1, String choix2, String choix3, String choix4, char rep, String explication) {  // 4 choix
+        QuestionVide q = new QuestionVide();
+        q.question = question;
+        q.choix = new String[4];
+        q.choix[0] = choix1;
+        q.choix[1] = choix2;
+        q.choix[2] = choix3;
+        q.choix[3] = choix4;
+        q.rep = rep;
+        q.explication = explication;re de cases aléatoire.");
         } else if (caseactuelle == MINIJEU) {
             print("Mini-Jeu!");
         } else {
@@ -213,6 +284,14 @@ class CultureParty extends Program {
         System.out.flush();
     }
 
+    void afficherTexte(String s) {
+        int len = length(s);
+        for (int i=0;i<len;i++) {
+            print(charAt(s, i));
+            delay(50);
+        }
+    }
+
     void algorithm() {
 
         // PARAMETRAGE
@@ -221,6 +300,10 @@ class CultureParty extends Program {
         for (int i=0;i<132/2-13;i++) print(" ");
         println("══════════════════════════");
         for (int i=0;i<132/2-14;i++) print(" ");
+        // ╔═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╦═════╗
+        // ║     ║     ║     ║     ║     ║     ║     ║     ║     ║     ║
+        // ║     ║     ║     ║     ║     ║     ║     ║     ║     ║     ║
+        // ╚═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╩═════╝
         println("Bienvenue dans Culture Party");
         for (int i=0;i<132/2-13;i++) print(" ");
         println("══════════════════════════");
@@ -231,7 +314,13 @@ class CultureParty extends Program {
         for (int i=0;i<132/2-14;i++) print(" ");
         println("10     |     15     |     20");
         int taillemap = 0;
-        while (taillemap != 10 && taillemap != 15 && taillemap != 20) taillemap = readInt();
+        do {
+            try {
+                taillemap = readInt();
+            } catch (Exception e) {
+                print("Erreur. ");
+            }
+        } while (taillemap != 10 && taillemap != 15 && taillemap != 20);
         char[] map = creerMapRoute66(taillemap);
         clearTerminal();
 
@@ -242,6 +331,9 @@ class CultureParty extends Program {
         println("3       |       6");
         int taillede = 0, resde;
         while (taillede != 3 && taillede != 6) taillede = readInt();
+        clearTerminal();
+
+
 
         // JEU
         while (joueur.position < taillemap - 1) {
